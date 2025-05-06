9ff329153e9df59b92f1b73ff6ab1be98dbadaba
Marisol Alaro <marisolalaro23@gmail.com>
2025-05-06 00:33:10 -0400

    arreglos de index

---
+++ app.js
@@ -0,0 +1,17 @@
+// var nombre = 'Marisol';
+// console.log(nombre);
+// console.log(typeof(nombre));
+
+// var edad = 34;
+// console.log(edad);
+// console.log(typeof(edad));
+
+// var sueldo = 1900.99;
+// console.log(sueldo);
+// console.log(typeof(sueldo));
+
+
+// var tieneTrabajo = false;
+// console.log(tieneTrabajo);
+// console.log(typeof(tieneTrabajo));
+
---
+++ funciones.js
@@ -0,0 +1,42 @@
+// var factorial = function(n){
+//     if((n===0) || (n ===1))
+//         return 1;
+//     else
+//         return(n * factorial(n -1));
+// }
+
+// console.log(factorial(4));
+
+// funcion de slaudar 
+// function saludar (nombre){
+//     console.log("hola "+ nombre+ "!!!");
+// }
+
+// saludar("marisol");
+
+// console.log(saludar);
+
+
+// funcion de suma de dos numeros
+// function sumar(a,b){
+//     return a+b;
+// }
+
+// console.log(sumar(5,5));
+
+// funcion par aver si un numeroe s par 
+
+// function par(n){
+// return n % 2 ===0;
+// }
+
+// console.log('El munero es par '+par(5));
+// console.log('El munero es par '+par(4));
+
+// function area(base, altura){
+//     return base* altura;
+// }
+
+// console.log(area(4,6));
+
+
